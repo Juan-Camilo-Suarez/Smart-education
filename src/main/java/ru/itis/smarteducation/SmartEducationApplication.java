0116c778a67b2d947d8deb304d1618c79b4c83f1
juancamilosuarez3 <juancamilosuarez3@gmail.com>
2023-03-10 21:57:37 +0300

    +++ initial Spring boot

---
+++ src/main/java/ru/itis/smarteducation/SmartEducationApplication.java
@@ -0,0 +1,13 @@
+package ru.itis.smarteducation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEducationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartEducationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/itis/smarteducation/SmartEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itis.smarteducation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEducationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
